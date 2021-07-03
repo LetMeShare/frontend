020388cd3bbc2cac823a0a7e5f5ce64a66baab6f
kishankr7979 <41482800+kishankr7979@users.noreply.github.com>
2021-07-04 01:07:55 +0530

    Server changes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://letsshare.herokuapp.com";
+const baseURL = "https://letssharefile.herokuapp.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
